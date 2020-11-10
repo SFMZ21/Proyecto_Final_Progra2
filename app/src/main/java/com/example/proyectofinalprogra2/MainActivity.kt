63823e701731e6ac906b26567c4abfd95fc641da
Lighnio <49454955+lighnio@users.noreply.github.com>
2020-11-10 00:57:02 -0600

    Inicio de la reestructuración
    Cambio de diseño completo

--- app/src/main/java/com/example/proyectofinalprogra2/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinalprogra2/MainActivity.kt
@@ -1,60 +1,64 @@
 package com.example.proyectofinalprogra2
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.firestore.FirebaseFirestore
+import android.view.LayoutInflater
+import android.widget.TextView
+import com.example.proyectofinalprogra2.fragments.adapters.ViewPagerAdapter
+import com.example.proyectofinalprogra2.fragments.fragmento_cuatro_movimientos
+import com.example.proyectofinalprogra2.fragments.fragmento_dos_ingresos
+import com.example.proyectofinalprogra2.fragments.fragmento_tres_gastos
+import com.example.proyectofinalprogra2.fragments.fragmento_uno_balance
 import kotlinx.android.synthetic.main.activity_main.*
-
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
-    val manager = supportFragmentManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val transaccion = manager.beginTransaction()
-        transaccion.replace(R.id.frame_contenedor, Fragment_Inicio() )
-        transaccion.addToBackStack(null)
-        transaccion.commit()
-        frgIngresos()
-        frgGasto()
-        frgMovimientos()
-        frgInicio()
-        btn_inicio.setOnClickListener{frgInicio()}
-        btn_ingreso.setOnClickListener{ frgIngresos()}
-        btn_gasto.setOnClickListener{frgGasto()}
-        btn_movimientos.setOnClickListener{frgMovimientos()}
+        setUpTabs()
+    }
 
+    fun setUpTabs(){
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragments(fragmento_uno_balance(), "")
+        adapter.addFragments(fragmento_dos_ingresos(), "")
+        adapter.addFragments(fragmento_tres_gastos(), "")
+        adapter.addFragments(fragmento_cuatro_movimientos(), "")
+        viewPager.adapter = adapter
+        tabs.setupWithViewPager(viewPager)
+
+        //Tabs personalizadas -> MALAS VISTAS
+        /**val txv_tab_inicio : TextView = LayoutInflater.from(this).inflate(R.layout.custom_tab, null) as TextView
+        txv_tab_inicio.setText("Inicio")
+        txv_tab_inicio.setCompoundDrawablesRelativeWithIntrinsicBounds(0, R.drawable.icn_principal_inicio, 0, 0)
+
+        val txv_tab_ingresos : TextView = LayoutInflater.from(this).inflate(R.layout.custom_tab, null) as TextView
+        txv_tab_ingresos.setText("Ingresos")
+        txv_tab_ingresos.setCompoundDrawablesRelativeWithIntrinsicBounds(0, R.drawable.icn_principal_ingresos, 0, 0)
+
+        val txv_tab_gastos : TextView = LayoutInflater.from(this).inflate(R.layout.custom_tab, null) as TextView
+        txv_tab_gastos.setText("Gastos")
+        txv_tab_gastos.setCompoundDrawablesRelativeWithIntrinsicBounds(0, R.drawable.icn_principal_gasto, 0, 0)
+
+        val txv_tab_movimientos : TextView = LayoutInflater.from(this).inflate(R.layout.custom_tab, null) as TextView
+        txv_tab_movimientos.setText("Movimientos")
+        txv_tab_movimientos.setCompoundDrawablesRelativeWithIntrinsicBounds(0, R.drawable.icn_principal_movimientos, 0, 0)**/
+
+        //Icons
+        tabs.getTabAt(0)!!.setIcon(R.drawable.icn_principal_inicio)
+        tabs.getTabAt(1)!!.setIcon(R.drawable.icn_principal_ingresos)
+        tabs.getTabAt(2)!!.setIcon(R.drawable.icn_principal_gasto)
+        tabs.getTabAt(3)!!.setIcon(R.drawable.icn_principal_movimientos)
+
+        /**tabs.getTabAt(0)!!.customView = (txv_tab_inicio)
+        tabs.getTabAt(1)!!.customView = (txv_tab_ingresos)
+        tabs.getTabAt(2)!!.customView = (txv_tab_gastos)
+        tabs.getTabAt(3)!!.customView = txv_tab_movimientos**/
 
 
     }
 
-    fun frgInicio(){
-        val transaccion = manager.beginTransaction()
-        transaccion.replace(R.id.frame_contenedor, Fragment_Inicio() )
-        transaccion.addToBackStack(null)
-        transaccion.commit()
-    }
-    fun frgIngresos(){
-        val transaccion = manager.beginTransaction()
-        transaccion.replace(R.id.frame_contenedor, fragment_Ingresos() )
-        transaccion.addToBackStack(null)
-        transaccion.commit()
-        }
-    fun frgGasto(){
-        val transaccion = manager.beginTransaction()
-        transaccion.replace(R.id.frame_contenedor, fragment_Gasto() )
-        transaccion.addToBackStack(null)
-        transaccion.commit()
-    }
-    fun frgMovimientos(){
-        val transaccion = manager.beginTransaction()
-        transaccion.replace(R.id.frame_contenedor, Fragment_Movimientos() )
-        transaccion.addToBackStack(null)
-        transaccion.commit()
-    }
-
-
-    }
-
-
+}
---
+++ app/src/main/java/com/example/proyectofinalprogra2/fragments/fragmento_tres_gastos.kt
@@ -0,0 +1,25 @@
+package com.example.proyectofinalprogra2.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.proyectofinalprogra2.R
+
+// TODO: Rename parameter arguments, choose names that match
+class fragmento_tres_gastos : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragmento_tres_gastos, container, false)
+    }
+    
+}
